a4b606d04d6159939e92b7c016840963213cf01c
mephisto <mephisto@a2e5ad42-ccce-4154-a8df-8a7cfdfd1d97>
2010-05-07 08:32:05 +0000

    Firmware/WiiSupport/: created script "update_firmware_update.bat" taking the version no. as argument, replacing "copy_firmware.bat" and "copy_smd_firmware.bat"
    git-svn-id: https://repository.combo-convention.de/dualstrike@44 a2e5ad42-ccce-4154-a8df-8a7cfdfd1d97

--- Firmware/WiiSupport/copy_smd_firmware.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-copy default\DualStrike.hex firmware_update\dual_strike_smd_firmware_V%1.hex
---
+++ Firmware/WiiSupport/update_firmware_update.bat
@@ -0,0 +1,21 @@
+@ECHO OFF
+copy build\dual_strike_firmware.hex firmware_update\dual_strike_firmware_V%1.hex
+copy build\dual_strike_smd_firmware.hex firmware_update\dual_strike_smd_firmware_V%1.hex
+copy build\dual_strike_firmware_wii.hex firmware_update\dual_strike_firmware_V%1_wii.hex
+copy build\dual_strike_smd_firmware_wii.hex firmware_update\dual_strike_smd_firmware_V%1_wii.hex
+
+ECHO @ECHO OFF>firmware_update\update_firmware.bat
+ECHO updatefw.exe -r "dual_strike_firmware_V%1.hex">>firmware_update\update_firmware.bat
+ECHO Pause>>firmware_update\update_firmware.bat
+
+ECHO @ECHO OFF>firmware_update\update_firmware_smd.bat
+ECHO updatefw.exe -r "dual_strike_smd_firmware_V%1.hex">>firmware_update\update_firmware_smd.bat
+ECHO Pause>>firmware_update\update_firmware_smd.bat
+
+ECHO @ECHO OFF>firmware_update\update_firmware_wii.bat
+ECHO updatefw.exe -r "dual_strike_firmware_V%1_wii.hex">>firmware_update\update_firmware_wii.bat
+ECHO Pause>>firmware_update\update_firmware_wii.bat
+
+ECHO @ECHO OFF>firmware_update\update_firmware_smd_wii.bat
+ECHO updatefw.exe -r "dual_strike_smd_firmware_V%1_wii.hex">>firmware_update\update_firmware_smd_wii.bat
+ECHO Pause>>firmware_update\update_firmware_smd_wii.bat
