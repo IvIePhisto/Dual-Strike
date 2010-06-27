aa2d1098dde2c5ed2574f4bab1a15af528d7e59d
mephisto <mephisto@a2e5ad42-ccce-4154-a8df-8a7cfdfd1d97>
2010-06-27 17:56:01 +0000

    Configuration/configuration.xml: renamed to configuration-dual_strike-v1.xml Configuration/configuration2header-file.xsl: started with header file generation Configuration: modified schema
    git-svn-id: https://repository.combo-convention.de/dualstrike@116 a2e5ad42-ccce-4154-a8df-8a7cfdfd1d97

---
+++ Configuration/configuration2header-file.xsl
@@ -0,0 +1,94 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:c="urn:dualstrike:configuration">
+  <xsl:output method="text" encoding="ascii"/>
+
+	<xsl:template match="/">
+		<xsl:apply-templates select="c:configuration"/>
+	</xsl:template>
+  
+  <xsl:template match="c:configuration">
+    
+  </xsl:template>
+  
+  <xsl:template name="define">
+    <xsl:param name="name"/>
+    <xsl:param name="value"/>
+    <xsl:text>#define </xsl:text>
+    <xsl:value-of select="$name"/>
+    <xsl:text> </xsl:text>
+    <xsl:value-of select="$value"/>
+    <xsl:text>&#xa;</xsl:text>
+  </xsl:template>
+  
+  <xsl:template name="shift-left">
+    <xsl:param name="value"/>
+    <xsl:param name="count"/>
+    <xsl:text>(</xsl:text>
+    <xsl:value-of select="$value"/>
+    <xsl:text>&lt;&lt;</xsl:text>
+    <xsl:value-of select="$count"/>
+    <xsl:text>)</xsl:text>
+  </xsl:template>
+  
+  <xsl:template name="array-accessor">
+    <xsl:param name="array"/>
+    <xsl:param name="array-index"/>
+    <xsl:value-of select="$array"/>
+    <xsl:text>[</xsl:text>
+    <xsl:value-of select="array-index"/>
+    <xsl:text>]</xsl:text>
+  </xsl:template>
+  
+  <xsl:template name="set-array-bit">
+    <xsl:param name="array"/>
+    <xsl:param name="array-index"/>
+    <xsl:param name="value"/>
+    <xsl:param name="bit-index"/>
+    
+    <xsl:call-template name="array-accessor">
+      <xsl:with-param name="array" select="$array"/>
+      <xsl:with-param name="array-index" select="$array-index"/>
+    </xsl:call-template>
+    
+    <xsl:text> </xsl:text>
+    
+    <xsl:choose>
+      <xsl:when test="boolean($value)">|= </xsl:when>
+      <xsl:otherwise>&amp;= ^</xsl:otherwise>
+    </xsl:choose>
+    
+    <xsl:call-template name="shift-left">
+      <xsl:with-param name="value" select="1"/>
+      <xsl:with-param name="count" select="$bit-index"/>
+    </xsl:call-template>
+  </xsl:template>
+
+  <xsl:template name="set-array-bits">
+    <xsl:param name="array"/>
+    <xsl:param name="array-index"/>
+    <xsl:param name="value"/>
+    <xsl:param name="bit-index"/>
+
+    <xsl:variable name="value-text">
+      <xsl:call-template name="shift-left">
+        <xsl:with-param name="value" select="$value"/>
+        <xsl:with-param name="count" select="$bit-index"/>
+      </xsl:call-template>
+    </xsl:variable>
+    <xsl:variable name="array-accessor">
+      <xsl:call-template name="array-accessor">
+        <xsl:with-param name="array" select="$array"/>
+        <xsl:with-param name="array-index" select="$array-index"/>
+      </xsl:call-template>
+    </xsl:variable>
+
+    <xsl:value-of select="$array-accessor"/>    
+    <xsl:text> |= </xsl:text>
+    <xsl:value-of select="$value-text"/>
+    <xsl:text>; </xsl:text>
+    <xsl:value-of select="$array-accessor"/>    
+    <xsl:text> &amp;= ^</xsl:text>
+    <xsl:value-of select="$value-text"/>
+    <xsl:text>;</xsl:text>
+  </xsl:template>
+</xsl:stylesheet>
--- Configuration/java/dualstrike/configuration/model/Page.java
+++ Configuration/java/dualstrike/configuration/model/Page.java
@@ -13,27 +13,27 @@ import java.util.List;
 import javax.annotation.Generated;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlID;
-import javax.xml.bind.annotation.XmlSchemaType;
-import javax.xml.bind.annotation.XmlSeeAlso;
+import javax.xml.bind.annotation.XmlElements;
 import javax.xml.bind.annotation.XmlType;
-import javax.xml.bind.annotation.adapters.CollapsedStringAdapter;
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 
 /**
- * <p>Java class for Setting complex type.
+ * <p>Java class for Page complex type.
  * 
  * <p>The following schema fragment specifies the expected content contained within this class.
  * 
  * <pre>
- * &lt;complexType name="Setting">
+ * &lt;complexType name="Page">
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
- *       &lt;group ref="{urn:strike-devices:configuration}Infos"/>
- *       &lt;attribute name="id" type="{http://www.w3.org/2001/XMLSchema}ID" />
+ *       &lt;sequence>
+ *         &lt;group ref="{urn:strike-devices:configuration}Infos"/>
+ *         &lt;choice maxOccurs="unbounded">
+ *           &lt;element name="choice" type="{urn:strike-devices:configuration}ChoiceSetting"/>
+ *           &lt;element name="boolean" type="{urn:strike-devices:configuration}BooleanSetting"/>
+ *         &lt;/choice>
+ *       &lt;/sequence>
  *     &lt;/restriction>
  *   &lt;/complexContent>
  * &lt;/complexType>
@@ -42,15 +42,13 @@ import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "Setting", propOrder = {
+@XmlType(name = "Page", propOrder = {
     "title",
-    "help"
-})
-@XmlSeeAlso({
-    BooleanSetting.class
+    "help",
+    "choiceOrBoolean"
 })
 @Generated(value = "com.sun.tools.internal.xjc.Driver", date = "2010-06-27T07:32:12+02:00", comments = "JAXB RI vJAXB 2.1.10 in JDK 6")
-public class Setting {
+public class Page {
 
     @XmlElement(required = true)
     @Generated(value = "com.sun.tools.internal.xjc.Driver", date = "2010-06-27T07:32:12+02:00", comments = "JAXB RI vJAXB 2.1.10 in JDK 6")
@@ -58,12 +56,12 @@ public class Setting {
     @XmlElement(required = true)
     @Generated(value = "com.sun.tools.internal.xjc.Driver", date = "2010-06-27T07:32:12+02:00", comments = "JAXB RI vJAXB 2.1.10 in JDK 6")
     protected List<Info> help;
-    @XmlAttribute
-    @XmlJavaTypeAdapter(CollapsedStringAdapter.class)
-    @XmlID
-    @XmlSchemaType(name = "ID")
+    @XmlElements({
+        @XmlElement(name = "boolean", type = BooleanSetting.class),
+        @XmlElement(name = "choice", type = ChoiceSetting.class)
+    })
     @Generated(value = "com.sun.tools.internal.xjc.Driver", date = "2010-06-27T07:32:12+02:00", comments = "JAXB RI vJAXB 2.1.10 in JDK 6")
-    protected String id;
+    protected List<Object> choiceOrBoolean;
 
     /**
      * Gets the value of the title property.
@@ -126,29 +124,34 @@ public class Setting {
     }
 
     /**
-     * Gets the value of the id property.
+     * Gets the value of the choiceOrBoolean property.
      * 
-     * @return
-     *     possible object is
-     *     {@link String }
-     *     
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the choiceOrBoolean property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getChoiceOrBoolean().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link BooleanSetting }
+     * {@link ChoiceSetting }
+     * 
+     * 
      */
     @Generated(value = "com.sun.tools.internal.xjc.Driver", date = "2010-06-27T07:32:12+02:00", comments = "JAXB RI vJAXB 2.1.10 in JDK 6")
-    public String getId() {
-        return id;
-    }
-
-    /**
-     * Sets the value of the id property.
-     * 
-     * @param value
-     *     allowed object is
-     *     {@link String }
-     *     
-     */
-    @Generated(value = "com.sun.tools.internal.xjc.Driver", date = "2010-06-27T07:32:12+02:00", comments = "JAXB RI vJAXB 2.1.10 in JDK 6")
-    public void setId(String value) {
-        this.id = value;
+    public List<Object> getChoiceOrBoolean() {
+        if (choiceOrBoolean == null) {
+            choiceOrBoolean = new ArrayList<Object>();
+        }
+        return this.choiceOrBoolean;
     }
 
 }
