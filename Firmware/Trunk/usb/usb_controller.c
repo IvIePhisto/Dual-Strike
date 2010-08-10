cb70a9992a9559e8f677336c1d76b1b0519cad8b
mephisto <mephisto@a2e5ad42-ccce-4154-a8df-8a7cfdfd1d97>
2010-08-10 16:48:05 +0000

    Firmware/Trunk/: removed HALT support from USB handling
    git-svn-id: https://repository.combo-convention.de/dualstrike@293 a2e5ad42-ccce-4154-a8df-8a7cfdfd1d97

--- Firmware/Trunk/usb/usb_controller.c
+++ Firmware/Trunk/usb/usb_controller.c
@@ -178,7 +178,6 @@ usbMsgLen_t usbFunctionSetup(uchar receivedData[8]) {
 		break;
 
 	case USB_MODE_XBOX:
-		/*
 	    if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS) {    // class request
 			if(rq->bRequest == USBRQ_HID_GET_REPORT) {
 		        if(reportID == EEPROM_SIZE_QUERY_REPORT_ID) {
@@ -188,18 +187,13 @@ usbMsgLen_t usbFunctionSetup(uchar receivedData[8]) {
 				}
 			}
 		}
-		else */if ((rq-> bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_VENDOR) {
+		else if ((rq-> bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_VENDOR) {
 			if(rq->bRequest == 0x06) {
 
 				usbMsgPtr = &data.array[20];
 
 				return 16;
 			}
-		}
-		else {
-			usbMsgPtr = data.array;
-
-			return 20;
 		}
 	}
 
