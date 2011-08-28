15c8d1d624c06b10012eab4493a6b4f95ad34e21
mephisto <mephisto@a2e5ad42-ccce-4154-a8df-8a7cfdfd1d97>
2011-08-28 20:00:02 +0000

    Firmware/MemoryTest/test/*.bat: updated paths
    git-svn-id: https://repository.combo-convention.de/dualstrike@438 a2e5ad42-ccce-4154-a8df-8a7cfdfd1d97

--- Firmware/MemoryTest/test/memory_test_atmega168.bat
+++ Firmware/MemoryTest/test/memory_test_atmega168.bat
@@ -8,10 +8,10 @@ ECHO .
 ECHO Put your Dual Strike into firmware update mode by plugging it in while
 ECHO holding Start.
 PAUSE
-MemoryTest/bootloadHID -r MemoryTest/firmware_atmega168_0x00-filled.hex
-MemoryTest/memoryTool>>report.txt
-MemoryTest/memoryTool -de eeprom_backup.shex
-MemoryTest/memoryTool -df flash.shex
+MemoryTest\bootloadHID -r MemoryTest\firmware_atmega168_0x00-filled.hex
+MemoryTest\memoryTool>>report.txt
+MemoryTest\memoryTool -de eeprom_backup.shex
+MemoryTest\memoryTool -df flash.shex
 ECHO Firmware ATmega168 0x00-filled>>report.txt
 ECHO ==============================>>report.txt
 memoryTool -c flash.shex firmware_atmega168_0x00-filled.shex>>report.txt
@@ -19,28 +19,28 @@ memoryTool -c flash.shex firmware_atmega168_0x00-filled.shex>>report.txt
 ECHO Put your Dual Strike into firmware update mode by plugging it in while
 ECHO holding Start.
 PAUSE
-MemoryTest/bootloadHID -r MemoryTest/firmware_atmega168_0xFF-filled.hex
-MemoryTest/memoryTool -df flash.shex
+MemoryTest\bootloadHID -r MemoryTest\firmware_atmega168_0xFF-filled.hex
+MemoryTest\memoryTool -df flash.shex
 ECHO.>>report.txt
 ECHO Firmware ATmega168 0xFF-filled>>report.txt
 ECHO ==============================>>report.txt
-MemoryTest/memoryTool -c flash.shex MemoryTest/firmware_atmega168_0xFF-filled.shex>>report.txt
+MemoryTest\memoryTool -c flash.shex MemoryTest\firmware_atmega168_0xFF-filled.shex>>report.txt
 
-memoryTool -e MemoryTest/eeprom_0x00-filled.hex
+memoryTool -e MemoryTest\eeprom_0x00-filled.hex
 memoryTool -de eeprom.shex
 ECHO.>>report.txt
 ECHO EEPROM 0x00-filled>>report.txt
 ECHO ==================>>report.txt
-MemoryTest/memoryTool -c eeprom.shex MemoryTest/eeprom_0x00-filled.shex>>report.txt
+MemoryTest\memoryTool -c eeprom.shex MemoryTest\eeprom_0x00-filled.shex>>report.txt
 
-memoryTool -e MemoryTest/eeprom_0xFF-filled.hex
+memoryTool -e MemoryTest\eeprom_0xFF-filled.hex
 memoryTool -de eeprom.shex
 ECHO.>>report.txt
 ECHO EEPROM 0xFF-filled>>report.txt
 ECHO ==================>>report.txt
-MemoryTest/memoryTool -c eeprom.shex MemoryTest/eeprom_0xFF-filled.shex>>report.txt
+MemoryTest\memoryTool -c eeprom.shex MemoryTest\eeprom_0xFF-filled.shex>>report.txt
 
-MemoryTest/memoryTool -e eeprom_backup.shex
+MemoryTest\memoryTool -e eeprom_backup.shex
 del eeprom_backup.shex
 del eeprom.shex
 del flash.shex
--- Firmware/MemoryTest/test/memory_test_atmega8.bat
+++ Firmware/MemoryTest/test/memory_test_atmega8.bat
@@ -8,12 +8,12 @@ ECHO .
 ECHO Put your Dual Strike into firmware update mode by plugging it in and holding 
 ECHO Start until firmware update is finished.
 PAUSE
-MemoryTest/bootloadHID -r MemoryTest/firmware_atmega8_0x00-filled.hex
+MemoryTest\bootloadHID -r MemoryTest\firmware_atmega8_0x00-filled.hex
 ECHO Leave firmware update mode by releasing Start.
 PAUSE
-MemoryTest/memoryTool>>report.txt
-MemoryTest/memoryTool -de eeprom_backup.shex
-MemoryTest/memoryTool -df flash.shex
+MemoryTest\memoryTool>>report.txt
+MemoryTest\memoryTool -de eeprom_backup.shex
+MemoryTest\memoryTool -df flash.shex
 ECHO Firmware ATmega8 0x00-filled>>report.txt
 ECHO ==============================>>report.txt
 memoryTool -c flash.shex firmware_atmega8_0x00-filled.shex>>report.txt
@@ -21,30 +21,30 @@ memoryTool -c flash.shex firmware_atmega8_0x00-filled.shex>>report.txt
 ECHO Put your Dual Strike into firmware update mode by plugging it in and holding 
 ECHO Start until firmware update is finished.
 PAUSE
-MemoryTest/bootloadHID -r MemoryTest/firmware_atmega8_0xFF-filled.hex
+MemoryTest\bootloadHID -r MemoryTest\firmware_atmega8_0xFF-filled.hex
 ECHO Leave firmware update mode by releasing Start.
 PAUSE
-MemoryTest/memoryTool -df flash.shex
+MemoryTest\memoryTool -df flash.shex
 ECHO.>>report.txt
 ECHO Firmware ATmega8 0xFF-filled>>report.txt
 ECHO ==============================>>report.txt
-MemoryTest/memoryTool -c flash.shex MemoryTest/firmware_atmega8_0xFF-filled.shex>>report.txt
+MemoryTest\memoryTool -c flash.shex MemoryTest\firmware_atmega8_0xFF-filled.shex>>report.txt
 
-memoryTool -e MemoryTest/eeprom_0x00-filled.hex
+memoryTool -e MemoryTest\eeprom_0x00-filled.hex
 memoryTool -de eeprom.shex
 ECHO.>>report.txt
 ECHO EEPROM 0x00-filled>>report.txt
 ECHO ==================>>report.txt
-MemoryTest/memoryTool -c eeprom.shex MemoryTest/eeprom_0x00-filled.shex>>report.txt
+MemoryTest\memoryTool -c eeprom.shex MemoryTest\eeprom_0x00-filled.shex>>report.txt
 
-memoryTool -e MemoryTest/eeprom_0xFF-filled.hex
+memoryTool -e MemoryTest\eeprom_0xFF-filled.hex
 memoryTool -de eeprom.shex
 ECHO.>>report.txt
 ECHO EEPROM 0xFF-filled>>report.txt
 ECHO ==================>>report.txt
-MemoryTest/memoryTool -c eeprom.shex MemoryTest/eeprom_0xFF-filled.shex>>report.txt
+MemoryTest\memoryTool -c eeprom.shex MemoryTest\eeprom_0xFF-filled.shex>>report.txt
 
-MemoryTest/memoryTool -e eeprom_backup.shex
+MemoryTest\memoryTool -e eeprom_backup.shex
 del eeprom_backup.shex
 del eeprom.shex
 del flash.shex
